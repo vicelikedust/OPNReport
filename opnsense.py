b05569250b04abe6485cd03fee68d5f950fba68d
Andy Binder <AndyBinder@gmx.de>
2019-03-11 11:19:54 +0100

    Time is float...

--- opnsense.py
+++ opnsense.py
@@ -58,7 +58,7 @@ class OpnSenseTimestamp(OpnSenseNode):
     datetime = None
 
     def __call__(self, content):
-        self.datetime = datetime.fromtimestamp(int(content), timezone.utc)
+        self.datetime = datetime.fromtimestamp(float(content), timezone.utc)
 
     @property
     def data(self):
